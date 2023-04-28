08121c3873f6fee587d324242bcd6aebd7caf08b
Leonor <ldominoni@gmail.com>
2023-04-27 21:05:49 -0300

    title

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { PostComponent } from './components/post/post.component';
 import { TweetComponent } from './components/tweet/tweet.component';
 import { HomeComponent } from './pages/home/home.component';
+import { NewPostComponent } from './components/new-post/new-post.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    PostComponent,
     TweetComponent,
-    HomeComponent
+    HomeComponent,
+    NewPostComponent
   ],
   imports: [
     BrowserModule,
